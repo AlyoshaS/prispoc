a4aa8c94b20b1b23e044caae04ea4d0259bc4cf9
Evelyn Schmitz <evelynschmitz1@gmail.com>
2022-08-03 16:16:13 -0300

    👷 initial configs

---
+++ .slicemachine/assets/slices/Formulario/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Formulario';
+
+export default {
+  title: 'slices/Formulario'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"formulario","items":[],"primary":{"title":[{"type":"heading1","text":"Reinvent value-added models","spans":[]}],"description":[{"type":"paragraph","text":"Culpa eiusmod amet cillum adipisicing quis Lorem reprehenderit. Ex labore proident fugiat eiusmod aute enim voluptate nisi aliquip qui labore. Ex do aute aliqua incididunt.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
---
+++ .slicemachine/assets/slices/TextEdito/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TextEdito';
+
+export default {
+  title: 'slices/TextEdito'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"text_edito","items":[],"primary":{"title":[{"type":"heading1","text":"Maximize out-of-the-box blockchains","spans":[]}],"description":[{"type":"paragraph","text":"Ut eiusmod laboris aliquip labore cillum.","spans":[]}],"TextEditorText":"exploit mission-critical e-commerce"},"id":"_Default"}} />
+_Default.storyName = 'Default'
